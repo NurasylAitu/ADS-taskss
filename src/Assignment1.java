feb6ff5768d3fbc66fb74eb0d5df1ca5d7666450
Nurassyl <242528@astanait.edu.kz>
2025-03-30 19:56:37 +0500

    Initial commit

---
+++ src/Assignment1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Assignment {
+    static int findMin(int[] arr, int index) {
+        if (index == arr.length - 1)
+            return arr[index];
+        return Math.min(arr[index], findMin(arr, index + 1));
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println();
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println();
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println(findMin(arr, 0));
+        scanner.close();
+    }
+}
+
